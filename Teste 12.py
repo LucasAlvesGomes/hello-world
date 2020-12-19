1d67f8440bd79a328c54b9d58c2ec3c3464cf4bf
Lucas Alves Gomes <76120454+LucasAlvesGomes@users.noreply.github.com>
2020-12-19 11:02:33 -0300

    painting wall
    Make a program that reads the width and height of a wall in meters, calculate its area and the amount of paint needed to paint it, knowing that each liter of paint paints an area of ​​2 square meters.

---
+++ Teste 12.py
@@ -0,0 +1,8 @@
+print('painting wall')
+
+width = float(input('width: '))
+height = float(input('height: '))
+area = width * height
+print(f'its area is:{area}')
+paint = area / 2
+print(f'to paint a wall, knowing that 1l of paint paint 2m², you need {paint}l of paint.')
